115b17779b2a1f4740bfca2ada67436aadfa722f
Farhaan Bukhsh <farhaan@opencraft.com>
2022-09-14 00:18:18 +0530

    fix: update question state
    Signed-off-by: Farhaan Bukhsh <farhaan@opencraft.com>

--- src/editors/containers/ProblemEditor/components/EditProblemView/QuestionWidget/index.jsx
+++ src/editors/containers/ProblemEditor/components/EditProblemView/QuestionWidget/index.jsx
@@ -1,19 +1,38 @@
 import React from 'react';
 import { Editor } from '@tinymce/tinymce-react';
-import { selectors } from '../../../../../data/redux';
-import { useSelector } from 'react-redux';
-
+import { selectors, actions } from '../../../../../data/redux';
+import { useSelector, useDispatch } from 'react-redux';
+import * as hooks from '../../../hooks';
 
 // This widget should be connected, grab all questions from store, update them as needed.
 export default function QuestionWidget() {
   const question = useSelector(selectors.problem.question);
+  const dispatch = useDispatch();
+  const { editorRef, refReady, setEditorRef } = hooks.prepareEditorRef();
+  const updateQuestionState = (dispatch, editorRef) => {
+    return (e) => {
+      debugger;
+      let content = editorRef.current.getContent();
+      console.log(content);
+      dispatch(actions.problem.updateQuestion(content));
+    }
+  }
+  if (!refReady) { return null; }
   return (
     <div>
       <div>
         <h1>
           Question
         </h1>
-        <Editor initialValue={question}/>
+        <Editor {
+          ...hooks.problemEditorConfig({
+            setEditorRef,
+            editorRef,
+            question,
+            dispatch,
+            updateQuestionState,
+          })
+        }/>
       </div>
     </div>
   );
